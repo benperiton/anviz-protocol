2f5a0b8b71c2e1d8f4df30457e1bbd11709be630
Ben Periton <git@ben.periton.co.uk>
2014-07-15 11:59:49 +0100

    Initial test to see if can connect via JS

---
+++ Src/Node/server.js
@@ -0,0 +1,14 @@
+/**
+ * @author Ben Periton <dev@ben.periton.co.uk>
+ *
+ * Listend to the Anviz A300 device for whenever someone checks in/out, so we
+ * get notified and can log that in the backoffice system.
+ */
+
+// ----- Header
+// -----------------------------------------------------------------------------
+
+var
+	// External dependencies
+	net = require('net')
+;
